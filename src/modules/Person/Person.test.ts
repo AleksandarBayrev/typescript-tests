977b5c7d7da132df008d7d448f1ac30c58fbfea4
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2020-03-21 18:12:15 +0200

    Update tests

--- src/modules/Person/Person.test.ts
+++ src/modules/Person/Person.test.ts
@@ -14,4 +14,18 @@ describe("Person", () => {
         expect(instance.Name).toEqual(name)
         expect(instance.Age).toEqual(age)
     })
+
+    it("Person.Introduce returns correct string", () => {        
+        const name: string = "Test person"
+        const age: number = 19
+        let instance: IPerson = new Person(name, age)
+        expect(instance.Introduce()).toEqual(`Hi! I'm ${name} and I'm ${age} years old`)
+    })
+
+    it("Person.Introduce returns correct string for 1 year old", () => {        
+        const name: string = "Test person"
+        const age: number = 1
+        let instance: IPerson = new Person(name, age)
+        expect(instance.Introduce()).toEqual(`Hi! I'm ${name} and I'm ${age} year old`)
+    })
 });
